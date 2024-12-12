868e04b3d486c408d2af990a5ffaa36c2a0ab3a4
Ayush Srivastava <sri.ayush10.as@gmail.com>
2024-12-13 03:42:33 +0530

    heapify and heapsort code added

--- trees/binary_search_trees/heap.py
+++ trees/binary_search_trees/heap.py
@@ -15,11 +15,39 @@ def kth_largest_element(nums, k):
     return min_heap[0]
 
 
+def heapify(arr, n, i):
+    # Max-heap formation
+    largest, left, right = i, 2 * i + 1, 2 * i + 2
+    if left < n and arr[largest] < arr[left]:
+        largest = left
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+    if largest != i:
+        # swap arr[largest] and arr[i]
+        arr[largest], arr[i] = arr[i], arr[largest]
+        heapify(arr, n, largest)
+
+
+def heapsort(nums, n):
+    for i in range(n // 2, -1, -1):
+        heapify(nums, n, i)
+    print("After heap formation:", nums)
+    # Extract elements from the heap one by one
+    for i in range(n - 1, 0, -1):
+        # Swap the root (largest element) with the last element
+        nums[0], nums[i] = nums[i], nums[0]
+        # Reduce the heap size and heapify the root
+        heapify(nums, i, 0)
+    print("After heap sort:", nums)
+
+
 if __name__ == "__main__":
-    nums, k = [3, 2, 1, 5, 6, 4], 2
-    print(kth_largest_element(nums, k))
+    nums, k = [-1, 13, 2, 1, 5, 6, 4], 2
+    print("k th largest element is:", kth_largest_element(nums, k))
     # Insert 1: Heap remains [2, 3] (no change, as 1 < 2).
     # Insert 5: Replace 2 → Heap becomes [3, 5] (cost O(logk)).
     # Insert 6: Replace 3 → Heap becomes [5, 6] (cost O(logk)).
     # Insert 4: Ignored, as 4 < 5
     # Final heap: [5, 6] ==> return 5
+    n = len(nums)
+    heapsort(nums, n)
