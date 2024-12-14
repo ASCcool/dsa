0b829399bbc99f4d0d086e6ebdff7ec41cdabec1
Ayush Srivastava <sri.ayush10.as@gmail.com>
2024-12-14 22:12:41 +0530

    top k frequent

---
+++ neetcode_150/topk_frequent.py
@@ -0,0 +1,18 @@
+from collections import OrderedDict
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Use Counter to count frequencies of elements in nums
+        freq_map = Counter(nums)
+        
+        # Sort the items by frequency in descending order
+        sorted_items = sorted(freq_map.items(), key=lambda x: x[1], reverse=True)
+        
+        # Extract the top k keys
+        return [item[0] for item in sorted_items[:k]]
+        
+
+if __name__ == "__main__":
+
+    # answer is [2,3]
+    topKFrequent([1,2,2,3,3,3], 2)
