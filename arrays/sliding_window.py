6dd77239c8d9e2e04c2d6efd585f0ac79f422a0a
Ayush Srivastava <sri.ayush10.as@gmail.com>
2025-01-21 02:05:18 +0530

    minimum-window-substring

--- arrays/sliding_window.py
+++ arrays/sliding_window.py
@@ -18,3 +18,51 @@ class Solution:
             charSet.add(s[r])
             res = max(res, r - l + 1)
         return res
+
+# Problem statement: https://leetcode.com/problems/minimum-window-substring/description/
+
+from collections import Counter
+
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        if not t or not s:
+            return ""
+        
+        # Count the frequency of characters in t
+        t_freq = Counter(t)
+        window_freq = {}
+        
+        # Variables to track the state of the window
+        have, need = 0, len(t_freq)
+        left = 0
+        min_len = float("inf")
+        res = ""
+        
+        # Expand the window by moving the right pointer
+        for right in range(len(s)):
+            char = s[right]
+            window_freq[char] = window_freq.get(char, 0) + 1
+            
+            # If the character is in t and its count matches t_freq
+            if char in t_freq and window_freq[char] == t_freq[char]:
+                have += 1
+            
+            # Contract the window until it's no longer valid
+            while have == need:
+                # Update the result if the current window is smaller
+                if (right - left + 1) < min_len:
+                    min_len = right - left + 1
+                    res = s[left:right + 1]
+                
+                # Remove the leftmost character from the window
+                window_freq[s[left]] -= 1
+                if s[left] in t_freq and window_freq[s[left]] < t_freq[s[left]]:
+                    have -= 1
+                left += 1
+        
+        return res
